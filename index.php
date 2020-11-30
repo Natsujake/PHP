254c4fea5a7362249698cdc01fc475e4ad6ab500
Natsujake <75273434+Natsujake@users.noreply.github.com>
2020-11-30 17:57:04 -0300

    atualizei?

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
 echo "<strong>Programando pela primeira vez</strong> :)"
-echo "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA BLZ"
+echo "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA BLZ. ola mundo"
 ?>
